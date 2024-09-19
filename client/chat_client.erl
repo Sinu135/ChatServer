248f4eabb03e5e1336a4631d95224ea87304c0ce
VP-21212581 <l21212581@tectijuana.edu.mx>
2024-09-19 16:27:25 -0700

    Modificar chat_client.erl

--- client/chat_client.erl
+++ client/chat_client.erl
@@ -1,14 +1,19 @@
 -module(chat_client).
 -export([start/0, send_message/2]).
 
+% Manera de ingresar
 start() ->
-    {ok, Socket} = gen_tcp:connect("localhost", 1234, [binary, {packet, 0}, {active, false}]),
+    % En gen_tcp:connect, lo primero es la dirección IP a la que se conecta, y lo segundo es el puerto
+    % Para ejecutar el comando en erlang, es {ok, Socket} = chat_client:start().
+    {ok, Socket} = gen_tcp:connect("25.29.49.242", 1234, [binary, {packet, 0}, {active, false}]),
     spawn(fun() -> receive_messages(Socket) end),
     {ok, Socket}.
 
+% Envío de mensajes a la red con el formato send_message(Socket, "Insertar mensaje aquí").
 send_message(Socket, Message) ->
     gen_tcp:send(Socket, Message).
 
+% Se ejecuta una vez se detecta que se envió un mensaje.
 receive_messages(Socket) ->
     case gen_tcp:recv(Socket, 0) of
         {ok, Message} ->
